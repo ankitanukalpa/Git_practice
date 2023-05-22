05d80ee5725c29c908d7d1ac4f03527f7bdbf4d1
ankitanukalpa <sathuaankit@gmail.com>
2023-05-22 09:19:43 +0530

    First commit

---
+++ Test.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Test {
+	public int returnAscii(String s) {
+		int a[]= {0,0,0,};
+		return 1;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
